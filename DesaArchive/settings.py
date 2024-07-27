dea5ed1032fb2fe53a5ccdd935f9fbd83ce4c48e
?0 <benjamindguntara@protonmail.com>
2024-07-27 20:50:54 +0700

    fdsfsf

--- DesaArchive/settings.py
+++ DesaArchive/settings.py
@@ -78,13 +78,6 @@ WSGI_APPLICATION = 'DesaArchive.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-'''
 if DEBUG:
     DATABASES = {
         'default': {
@@ -104,7 +97,6 @@ else:
             'PORT': '5432',
         }
     }
-'''
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
